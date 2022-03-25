8ae4e9b84dd459911508cb57eba467cba8c13a46
Dayane Núbia <dayane.nubia67@gmail.com>
2022-03-25 20:55:37 -0300

    Update Main.java

--- Ex/src/geometria/Main.java
+++ Ex/src/geometria/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	// Teste da classe Quadrado
 	Quadrado primeiroQuadrado = new Quadrado();
-	primeiroQuadrado.setLado(12);
+	primeiroQuadrado.setLado(15);
 	
 	double areaPrimeiroQuadrado = primeiroQuadrado.calculaArea();
 	System.out.println("Area quadrado: " + areaPrimeiroQuadrado);
@@ -13,32 +13,34 @@ public class Main {
 	primeiroQuadrado.desenhar();
 	
 	// Teste da classe Retangulo
-	Quadrado primeiroRetangulo = new Retangulo();
-	primeiroRetangulo.setLado(12);
+	Retangulo primeiroRetangulo = new Retangulo();
+	primeiroRetangulo.setAltura(15);
+	primeiroRetangulo.setBase(10);
 				
 	double areaPrimeiroRetangulo = primeiroRetangulo.calculaArea();
-	System.out.println("Area quadrado: " + areaPrimeiroRetangulo);
+	System.out.println("Area retangulo: " + areaPrimeiroRetangulo);
 				
-	System.out.println("Desenho quadrado: ");
+	System.out.println("Desenho retangulo: ");
 	primeiroRetangulo.desenhar();
 	
 	// Teste da classe Triangulo
-	Quadrado primeiroTriangulo = new Triangulo();
-	primeiroTriangulo.setLado(12);
+	Triangulo primeiroTriangulo = new Triangulo();
+	primeiroTriangulo.setBase(10);
+	primeiroTriangulo.setAltura(15);
 				
 	double areaPrimeiroTriangulo = primeiroTriangulo.calculaArea();
-	System.out.println("Area quadrado: " + areaPrimeiroTriangulo);
+	System.out.println("Area triangulo: " + areaPrimeiroTriangulo);
 				
-	System.out.println("Desenho quadrado: ");
+	System.out.println("Desenho triangulo: ");
 	primeiroTriangulo.desenhar();
 	
 	// Teste da classe Circulo
-	Quadrado primeiroCirculo = new Circulo();
-	primeiroCirculo.setLado(12);
+	Circulo primeiroCirculo = new Circulo();
+	primeiroCirculo.setRaio(12);
 				
 	double areaPrimeiroCirculo = primeiroQuadrado.calculaArea();
-	System.out.println("Area quadrado: " + areaPrimeiroCirculo);
+	System.out.println("Area circulo: " + areaPrimeiroCirculo);
 				
-	System.out.println("Desenho quadrado: ");
+	System.out.println("Desenho circulo: ");
 	primeiroCirculo.desenhar();
 }
